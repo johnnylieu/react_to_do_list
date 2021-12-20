d7917d4809c364d31a138183c628fdb8bbf3c2b4
Johnny Lieu <you@example.com>
2021-12-19 19:45:29 -0800

    form created and working

--- react-todo-app/src/components/TodoForm.js
+++ react-todo-app/src/components/TodoForm.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function TodoForm() {
+    const [input, setInput] = useState('')
+
     return (
-        <div>
-            
-        </div>
+        <form className="todo-form">
+            <input type="text" 
+            placeholder='Add a task' 
+            value={input} 
+            name="text" 
+            className="todo-input"
+            />
+            <button className='todo-button'>Add task</button>
+        </form>
     )
 }
 
