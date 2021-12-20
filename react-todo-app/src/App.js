825b950c80cc289d0c6521f0c7d6152c0c220cae
Johnny Lieu <you@example.com>
2021-12-19 19:33:33 -0800

    npx ready

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>To Do List</h1>
+    </div>
+  );
+}
+
+export default App;
