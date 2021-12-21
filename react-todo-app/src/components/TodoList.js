ce5a12b6f2cf4c0c3256d0a6357ce5663f6a4428
Johnny Lieu <you@example.com>
2021-12-21 15:01:05 -0800

    able to update task list now

--- react-todo-app/src/components/TodoList.js
+++ react-todo-app/src/components/TodoList.js
@@ -7,19 +7,27 @@ function TodoList() {
 
     const addTodo = todo => {
         if(!todo.text || /^\s*$/.test(todo.text)){
-            return
-        }
+            return;
+        };
 
         const newTodos = [todo, ...todos]
 
         setTodos(newTodos);
     };
 
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return;
+        };
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    };
+
     const removeTodo = id => {
-        const removeArr = [...todos].filter(todo => todo.id !== id)
+        const removeArr = [...todos].filter(todo => todo.id !== id);
 
         setTodos(removeArr);
-    }
+    };
 
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
@@ -35,7 +43,7 @@ function TodoList() {
         <div>
             <h1>What's the plan for today?</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
     )
 };
